372f7b59f70669d8421fbdc01ae652df9d13b9fd
rrw788 <rivallew231@gmail.com>
2025-02-01 12:14:46 +0700

    puri bakery Fix import and deployment issues 2.0

--- app.py
+++ app.py
@@ -1,24 +1,17 @@
-# app.py
 from flask import Flask
-from app.config import Config
+from app.config import Config  # Gunakan relative import
 from app.extensions import db, migrate
-from app.routes.routes import routes  # Perbaiki import ini
-from app import create_app
-
-app = create_app()
+from app.routes.routes import routes
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder="static")
     app.config.from_object(Config)
-    
+
     # Inisialisasi database dan migrasi
     db.init_app(app)
     migrate.init_app(app, db)
-    
-    # Register blueprint
-    app.register_blueprint(routes)  # Perbaiki ini
-    
-    return app
 
-if __name__ == "__main__":
-    app.run(debug=True)
+    # Register blueprint
+    app.register_blueprint(routes)
+
+    return app
